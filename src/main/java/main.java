4485cddff187568aea01272980a2890a0a20d977
ali_m <ali.murrar@stu.najah.edu>
2020-04-26 05:12:34 +0300

    ali

---
+++ src/main/java/main.java
@@ -0,0 +1,12 @@
+public class main {
+
+    public static void main(String []a)
+    {
+        System.out.println("dkljfgnb");
+
+    }
+    public static int add(int x , int y ){
+        return x+y;
+    }
+
+}
